089b256da2023e096e85c761762e3614ca1c5183
Samuel Costa <samuelmfa@gmail.com>
2020-10-19 07:07:45 -0300

    teste leitura arquivo json

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,60 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from './home.service';
 import { Tarefa } from './../models/tarefa.model';
+
+export interface Dados {
+  perfilA: string;
+  perfilB: string;
+  valorAlcada: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
   public listaTarefas: Tarefa[] | any;
+  public retornoUrl: any;
+  public chave = "url.hub.teste2";
+  public linkUrl: string;
+
+  public dados: Dados[] = [
+    {
+      perfilA: 'RH',
+      perfilB: 'RH',
+      valorAlcada: 1000
+
+    },
+    {
+      perfilA: 'FINACEIRO',
+      perfilB: 'DIRETORIA',
+      valorAlcada: 3000
+
+    },
+    {
+      perfilA: 'MARKETING',
+      perfilB: 'DIRETORIA',
+      valorAlcada: 6000
+
+    },
+    {
+      perfilA: 'COMERCIAL',
+      perfilB: 'RH',
+      valorAlcada: 5000
+
+    },
+
+    {
+      perfilA: 'MARKETING',
+      perfilB: 'DIRETORIA',
+      valorAlcada: 6000
+
+    },
+  ];
 
   constructor(
     public homeService: HomeService,
@@ -17,6 +62,27 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.buscarTarefas();
+    let result = this.validaDados(this.dados);
+    console.log(result);
+    this.buscaValorJson();
+  }
+
+  validaDados(dados: Dados[]): boolean {
+    let resultado = false;
+    let valores: Dados[] = [];
+
+    dados.forEach((comparador) => {
+      const valida = valores.find(elemento => elemento.perfilA === comparador.perfilA && elemento.perfilB === comparador.perfilB);
+
+      if (valida === undefined) {
+        valores.push(comparador);
+
+      } else {
+        resultado = true;
+      }
+    });
+    return resultado;
+
   }
 
   buscarTarefas(): void {
@@ -30,5 +96,14 @@ export class HomeComponent implements OnInit {
 
   }
 
+  buscaValorJson() {
+    this.homeService.getUrl().subscribe((resp: any) => {
+      console.log(this.carregaValor(resp, this.chave));
+    });
+  }
+
+  carregaValor(lista: any, chave: string) {
+    return this.linkUrl = lista[chave];
+  }
 
 }
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -3,20 +3,24 @@ import { Injectable } from '@angular/core';
 import { Tarefa } from '../models/tarefa.model';
 import { ApiService } from './../services/api.service';
 import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class HomeService {
+export class HomeService extends ApiService {
+
+  public urlJson = '../../static/url.json';
 
   constructor(
-    public apiService: ApiService
-  ) { }
+    public apiService: ApiService,
+    public http: HttpClient
+  ) { super(http) }
 
   public recebeAcao(evento: any) {
     switch (evento.acao) {
       case ('concluido'):
-        return this.editarTarefa(evento.tarefa, 'concluido');
+        return this.editarTarefaApi(evento.tarefa, 'concluido');
       case ('inicio'):
-        return this.editarTarefa(evento.tarefa, 'inicio');
+        return this.editarTarefaApi(evento.tarefa, 'inicio');
       case ('excluir'):
         return this.excluirTarefa(evento.tarefa);
       case ('atualizar'):
@@ -30,13 +34,18 @@ export class HomeService {
     return this.apiService.buscarTarefas();
   }
 
-  public editarTarefa(tarefa: Tarefa, status: string): Observable<any> {
+  public editarTarefaApi(tarefa: Tarefa, status: string): Observable<any> {
     tarefa.status = status;
     return this.apiService.editarTarefa(tarefa).pipe(map((response: any) => response));
   }
 
   public excluirTarefa(tarefa: Tarefa): Observable<any> {
-   return this.apiService.excluirTarefa(tarefa).pipe(map((response: any) => response));
+    return this.apiService.excluirTarefa(tarefa).pipe(map((response: any) => response));
+  }
+
+  public getUrl() {
+    return this.http.get(this.urlJson, this.header);
+
   }
 
 }
